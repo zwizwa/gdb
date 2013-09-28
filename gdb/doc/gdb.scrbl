af71681c2fb5738f40c3b7e0e259848304fc212c
Tom Schouten <tom@zwizwa.be>
2013-09-28 09:36:35 -0400

    initial commit - copy from RAI

---
+++ gdb/doc/gdb.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/doc
+@(require scribble/manual
+          scribble/eval)
+
+@(define-syntax-rule ($ id) (racket id))
+
+@title{GDB MI interface}
+
+No documentation yet.  See @vertbatim{gdb.rkt} in the
+@hyperlink["http://github.com/zwizwa/gdb" "github"] repository.
---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang setup/infotab
+(define name "gdb")
+(define blurb
+  '("GDB MI interface."))
+(define repositories '("4.x"))
+(define primary-file '("gdb.rkt"))
+(define homepage "http://github.com/zwizwa/gdb")
+(define categories '(devtools metaprogramming))
+(define scribblings '(("doc/gdb.scrbl" ())))
+(define release-notes
+  '(div
+    "See the "
+    (a ((href "http://github.com/zwizwa/gdb")) "github page")
+    " for more information."))
+(define version "1.0")
+(define deps (list))
